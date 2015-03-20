2fe0b9d1b4b2e850cd83c1221fe35122aa7ef0a6
wu736139669 <wu736139669@736139669@qq.com>
2015-03-20 16:18:46 +0800

    init

---
+++ Question/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Question
+//
+//  Created by xmfish on 15/3/20.
+//  Copyright (c) 2015年 ash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Question/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Question
+//
+//  Created by xmfish on 15/3/20.
+//  Copyright (c) 2015年 ash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
